fd4dd116c0e38556b70bce919c13c6c1dd9457c2
Ivana <ivanagbarreto@gmail.com>
2026-01-08 12:27:07 -0300

    changes in exercises

--- exercises.js
+++ exercises.js
@@ -1,28 +1,28 @@
-const objeto1 = {saludo:"hola mundo"}
+const objeto1 = { saludo: "hola mundo" };
 
 //console.log(objeto1.saludo)
 
 class Clase1 {
-
-    get(){return "hello world"}
-
+  get() {
+    return "hello world";
+  }
 }
-const instanciaClase1 = new Clase1()
-const instanciaClase2 = new Clase1()
+const instanciaClase1 = new Clase1();
+const instanciaClase2 = new Clase1();
 
 //console.log(instanciaClase1.get())
 
-const array1 = [1,2,3]
+const array1 = [1, 2, 3];
 
-//console.log(array1[0]) 
+//console.log(array1[0])
 
-array1.push(4)
+array1.push(4);
 
 //console.log(array1)
 
 //console.log(crypto.randomUUID());
-const opcion1 = true
-const opcion2 = false
+const opcion1 = true;
+const opcion2 = false;
 
 // console.log(true == opcion2)
 // console.log(1 == opcion2)
@@ -33,12 +33,31 @@ const opcion2 = false
 // console.log({nombre:"Helena"} == undefined)
 // console.log(undefined == undefined)
 
-const objeto2 = {nombre: "Helena", edad:8}
+const objeto2 = {
+  nombre: "Helena",
+  edad: 8,
+  color: () => {
+    return "gris y blanco";
+  }, //metodo
+};
 
-const {nombre} = objeto2
+class Clase2 {
+  color = () => {
+    return "gris y blanco";
+  };
+}
+const clase2 = new Clase2();
 
-console.log(nombre)
-console.log(objeto2.nombre)
+console.log(clase2.color());
+
+const { color } = objeto2;
+console.log(color());
+console.log(objeto2.color());
+
+const { nombre } = objeto2;
+
+console.log(nombre);
+console.log(objeto2.nombre);
 // console.log(objeto2.nombre)
 
 // console.log(true || "segundo")
@@ -46,12 +65,12 @@ console.log(objeto2.nombre)
 // console.log(false || "segundo")
 // console.log(undefined || "segundo")
 
-const ternaria1 = true ? "primero" : "segundo" 
-const ternaria2 = [].length ? "primero" : "segundo" 
+const ternaria1 = true ? "primero" : "segundo";
+const ternaria2 = [].length ? "primero" : "segundo";
 //console.log(ternaria1)
 
-const ternaria3 = true ?? "segundo"
-const ternaria4 = "primero" ?? "segundo"
+const ternaria3 = true ?? "segundo";
+const ternaria4 = "primero" ?? "segundo";
 
 //console.log(ternaria3)
 //console.log(ternaria4)
